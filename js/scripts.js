20c6dad1db6abfa8f9ca2fbaa2b0775f17632b93
Ollen Hopkins <hopkins.ollen@gmail.com>
2022-11-12 14:00:44 -0500

    Added jQuery for modals, added sass npm install, and added json script

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,13 @@ $(function () {
        
     });
     
+    $('#reserveButton').click(function(){
+      $('#reserveModal').modal('show');
+    });
+
+    $('.loginButton').click(function(){
+      $('#loginModal').modal('show');
+    });
+
  });
+
